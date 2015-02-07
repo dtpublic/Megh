a65be6831e46fdc413310d8887a06f9d3979eb30
Chandni Singh <chandni@datatorrent.com>
2015-02-06 18:40:54 -0800

    SPOI-4003: added retrying of failed blocks

--- src/main/java/com/datatorrent/apps/ingestion/io/input/BlockReader.java
+++ src/main/java/com/datatorrent/apps/ingestion/io/input/BlockReader.java
@@ -1,23 +1,101 @@
 package com.datatorrent.apps.ingestion.io.input;
 
 import java.io.IOException;
+import java.util.Map;
+import java.util.Queue;
 
+import javax.validation.constraints.NotNull;
+
+import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.collect.Lists;
+
+import com.datatorrent.api.DefaultOutputPort;
+import com.datatorrent.api.annotation.OutputPortFieldAnnotation;
+
+import com.datatorrent.common.util.Slice;
+import com.datatorrent.lib.io.block.AbstractBlockReader;
+import com.datatorrent.lib.io.block.BlockMetadata;
 import com.datatorrent.lib.io.block.FSSliceReader;
 
 public class BlockReader extends FSSliceReader
 {
-
+  @NotNull
   protected String directory; // Same as FileSpiltter directory.
+
+  protected int maxRetries;
+  protected Queue<FailedBlock> failedQueue;
+
+  @OutputPortFieldAnnotation(optional = true, error = true)
+  public final transient DefaultOutputPort<BlockMetadata.FileBlockMetadata> error = new DefaultOutputPort<BlockMetadata.FileBlockMetadata>();
+
+  public BlockReader()
+  {
+    super();
+    maxRetries = 0;
+    failedQueue = Lists.newLinkedList();
+  }
 
   @Override
   protected FileSystem getFSInstance() throws IOException
   {
     return FileSystem.newInstance(new Path(directory).toUri(), configuration);
+  }
+
+  @Override
+  protected boolean canHandleMoreBlocks()
+  {
+    return (!blockQueue.isEmpty() || !failedQueue.isEmpty()) && blocksPerWindow < threshold;
+  }
+
+  @Override
+  protected void processHeadBlock()
+  {
+    if (blockQueue.isEmpty() && !failedQueue.isEmpty()) {
+      FailedBlock failedBlock = failedQueue.poll();
+      failedBlock.retries++;
+      try {
+        processBlockMetadata(failedBlock);
+      }
+      catch (Throwable t) {
+        LOG.debug("attempt {} to process block {} failed", failedBlock.retries, failedBlock.block.getBlockId());
+        if(failedBlock.retries < maxRetries) {
+          failedQueue.add(failedBlock);
+        }
+        else if(error.isConnected()){
+          error.emit(failedBlock.block);
+        }
+      }
+    }
+    else {
+      BlockMetadata.FileBlockMetadata head = blockQueue.poll();
+      if (maxRetries == 0) {
+        processBlockMetadata(head);
+      }
+      else {
+        try {
+          processBlockMetadata(head);
+        }
+        catch (Throwable t) {
+          LOG.debug("failed to process block {}", head.getBlockId());
+          failedQueue.add(new FailedBlock(head));
+        }
+      }
+    }
+  }
+
+  @Override
+  public void partitioned(Map<Integer, Partition<AbstractBlockReader<Slice, BlockMetadata.FileBlockMetadata,
+    FSDataInputStream>>> integerPartitionMap)
+  {
+    super.partitioned(integerPartitionMap);
+    for (Partition<AbstractBlockReader<Slice, BlockMetadata.FileBlockMetadata, FSDataInputStream>> partition : integerPartitionMap.values()) {
+      ((BlockReader) partition.getPartitionedInstance()).directory = this.directory;
+    }
   }
 
   public String getDirectory()
@@ -31,5 +109,47 @@ public class BlockReader extends FSSliceReader
   }
 
   private static final Logger LOG = LoggerFactory.getLogger(BlockReader.class);
+
+  protected static class FailedBlock extends BlockMetadata.FileBlockMetadata
+  {
+    protected int retries;
+    protected final FileBlockMetadata block;
+
+    @SuppressWarnings("unused")
+    private FailedBlock()
+    {
+      //for kryo
+      block = null;
+    }
+
+    FailedBlock(BlockMetadata.FileBlockMetadata block)
+    {
+      this.block = block;
+    }
+
+    @Override
+    public long getBlockId()
+    {
+      return block.getBlockId();
+    }
+  }
+
+  /**
+   * Sets the max number of retries.
+   *
+   * @param maxRetries maximum number of retries
+   */
+  public void setMaxRetries(int maxRetries)
+  {
+    this.maxRetries = maxRetries;
+  }
+
+  /**
+   * @return the max number of retries.
+   */
+  public int getMaxRetries()
+  {
+    return this.maxRetries;
+  }
 
 }
---
+++ src/test/java/com/datatorrent/apps/ingestion/io/input/BlockReaderTest.java
@@ -0,0 +1,107 @@
+package com.datatorrent.apps.ingestion.io.input;
+
+import java.io.IOException;
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+import com.datatorrent.lib.io.block.BlockMetadata;
+import com.datatorrent.lib.io.block.FSSliceReader;
+import com.datatorrent.lib.io.block.FSSliceReaderTest;
+import com.datatorrent.lib.testbench.CollectorTestSink;
+import com.datatorrent.lib.util.TestUtils;
+
+public class BlockReaderTest extends FSSliceReaderTest
+{
+
+  @Override
+  protected FSSliceReader getBlockReader()
+  {
+    BlockReaderWhichFails reader = new BlockReaderWhichFails();
+    reader.setDirectory("test");
+    reader.setMaxRetries(5);
+    return reader;
+  }
+
+  @Override
+  public void testBytesReceived() throws IOException
+  {
+    //suppressing test
+  }
+
+  @Test
+  public void testFailedBlocks()
+  {
+    long blockSize = 1500;
+    int noOfBlocks = (int) ((testMeta.dataFile.length() / blockSize) + (((testMeta.dataFile.length() % blockSize) == 0) ? 0 : 1));
+    testMeta.blockReader.beginWindow(1);
+
+    for (int i = 0; i < noOfBlocks; i++) {
+      BlockMetadata.FileBlockMetadata blockMetadata = new BlockMetadata.FileBlockMetadata(testMeta.dataFile.getAbsolutePath(), i, i * blockSize,
+        i == noOfBlocks - 1 ? testMeta.dataFile.length() : (i + 1) * blockSize,
+        i == noOfBlocks - 1, i - 1);
+
+      testMeta.blockReader.blocksMetadataInput.process(blockMetadata);
+    }
+
+    testMeta.blockReader.endWindow();
+
+    Assert.assertEquals("num failed blocks", noOfBlocks, ((BlockReader) testMeta.blockReader).failedQueue.size());
+  }
+
+  @Test
+  public void testNumRetries()
+  {
+    CollectorTestSink<BlockMetadata.FileBlockMetadata> failedBlocks = new CollectorTestSink<BlockMetadata.FileBlockMetadata>();
+    TestUtils.setSink(((BlockReader) testMeta.blockReader).error, failedBlocks);
+
+    long blockSize = 1500;
+    int noOfBlocks = (int) ((testMeta.dataFile.length() / blockSize) + (((testMeta.dataFile.length() % blockSize) == 0) ? 0 : 1));
+
+    testFailedBlocks();
+
+    for (int i = 2; i < 7; i++) {
+      testMeta.blockReader.beginWindow(i);
+      int count = 0;
+      while (count++ < noOfBlocks) {
+        ((BlockReader) testMeta.blockReader).processHeadBlock();
+      }
+
+      testMeta.blockReader.endWindow();
+    }
+
+    Assert.assertEquals("max retries", noOfBlocks, failedBlocks.collectedTuples.size());
+  }
+
+  @Test
+  public void testIdleTimeHandlingOfFailedBlocks()
+  {
+    CollectorTestSink<BlockMetadata.FileBlockMetadata> failedBlocks = new CollectorTestSink<BlockMetadata.FileBlockMetadata>();
+    TestUtils.setSink(((BlockReader) testMeta.blockReader).error, failedBlocks);
+
+    long blockSize = 1500;
+    int noOfBlocks = (int) ((testMeta.dataFile.length() / blockSize) + (((testMeta.dataFile.length() % blockSize) == 0) ? 0 : 1));
+    testFailedBlocks();
+
+    testMeta.blockReader.beginWindow(2);
+
+    int count = 0;
+    while (count++ < (noOfBlocks * 5)) {
+      testMeta.blockReader.handleIdleTime();
+    }
+
+    testMeta.blockReader.endWindow();
+    Assert.assertEquals("process failed blocks in idle time", noOfBlocks, failedBlocks.collectedTuples.size());
+  }
+
+  private static class BlockReaderWhichFails extends BlockReader
+  {
+    @Override
+    protected void readBlock(BlockMetadata blockMetadata) throws IOException
+    {
+      throw new IOException();
+    }
+
+  }
+
+}
