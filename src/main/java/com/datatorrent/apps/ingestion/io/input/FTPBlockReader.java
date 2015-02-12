931547e2dd7d110b5737386173d7f0a054527cce
yogidevendra <devendra@datatorrent.com>
2015-02-12 12:22:27 +0530

    SPOI-3892 ftp input source not working 1. Added FTPBlockReaderContext 2. Setting readerContext for FTPBlockReader

--- src/main/java/com/datatorrent/apps/ingestion/io/input/FTPBlockReader.java
+++ src/main/java/com/datatorrent/apps/ingestion/io/input/FTPBlockReader.java
@@ -11,9 +11,19 @@ import org.slf4j.LoggerFactory;
 
 import com.datatorrent.apps.ingestion.io.DTFTPFileSystem;
 import com.datatorrent.lib.io.block.BlockMetadata;
+import com.datatorrent.lib.io.block.ReaderContext;
 
 public class FTPBlockReader extends BlockReader
 {
+
+  /**
+   * Sets FTP BlockReader Context
+   */
+  public FTPBlockReader()
+  {
+    super();
+    this.readerContext = new FTPBlockReaderContext();
+  }
 
   @Override
   protected FileSystem getFSInstance() throws IOException
@@ -32,4 +42,29 @@ public class FTPBlockReader extends BlockReader
 
   private static final Logger LOG = LoggerFactory.getLogger(FTPBlockReader.class);
 
+  /**
+   * BlockReadeContext for reading FTP Blocks.<br/>
+   * This should use read API without offset.
+   */
+  private static class FTPBlockReaderContext extends ReaderContext.FixedBytesReaderContext<FSDataInputStream>
+  {
+    @Override
+    protected Entity readEntity() throws IOException
+    {
+      entity.clear();
+      int bytesToRead = length;
+      if (offset + length >= blockMetadata.getLength()) {
+        bytesToRead = (int) (blockMetadata.getLength() - offset);
+      }
+      byte[] record = new byte[bytesToRead];
+      // If we use read call with offset then it will try to seek on FTP stream and throw exception
+      // Hence use read call without offset
+      // Offset handling is done using setRestartOffset in DTFTPFileSystem.java
+      stream.read(record, 0, bytesToRead);
+      entity.setUsedBytes(bytesToRead);
+      entity.setRecord(record);
+
+      return entity;
+    }
+  }
 }
