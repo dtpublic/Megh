7a712c71ab297bdb75d813b78745600bb2b12194
Chandni Singh <chandni@datatorrent.com>
2015-04-03 16:06:16 -0700

    implemented paralled partition strategy so that counters are not lost

--- src/main/java/com/datatorrent/apps/ingestion/io/BlockWriter.java
+++ src/main/java/com/datatorrent/apps/ingestion/io/BlockWriter.java
@@ -4,7 +4,10 @@
  */
 package com.datatorrent.apps.ingestion.io;
 
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.lang.mutable.MutableLong;
 import org.apache.hadoop.fs.Path;
@@ -13,11 +16,10 @@ import org.slf4j.LoggerFactory;
 
 import com.google.common.collect.Lists;
 
-import com.datatorrent.api.Context;
-import com.datatorrent.api.DefaultInputPort;
-import com.datatorrent.api.DefaultOutputPort;
+import com.datatorrent.api.*;
 
 import com.datatorrent.common.util.Slice;
+import com.datatorrent.lib.counters.BasicCounters;
 import com.datatorrent.lib.io.block.AbstractBlockReader;
 import com.datatorrent.lib.io.block.BlockMetadata;
 import com.datatorrent.lib.io.fs.AbstractFileOutputOperator;
@@ -27,12 +29,11 @@ import com.datatorrent.lib.io.fs.AbstractFileOutputOperator;
  *
  * @author Yogi/Sandeep
  */
-public class BlockWriter extends AbstractFileOutputOperator<AbstractBlockReader.ReaderRecord<Slice>>
+public class BlockWriter extends AbstractFileOutputOperator<AbstractBlockReader.ReaderRecord<Slice>> implements
+  Partitioner<BlockWriter>
 {
   public static final String SUBDIR_BLOCKS = "blocks";
   private transient List<BlockMetadata.FileBlockMetadata> blockMetadatas;
-
-  private transient long timePerWindow;
 
   public final transient DefaultInputPort<BlockMetadata.FileBlockMetadata> blockMetadataInput = new DefaultInputPort<BlockMetadata.FileBlockMetadata>()
   {
@@ -60,16 +61,6 @@ public class BlockWriter extends AbstractFileOutputOperator<AbstractBlockReader.
   {
     filePath = context.getValue(Context.DAGContext.APPLICATION_PATH) + Path.SEPARATOR + SUBDIR_BLOCKS;
     super.setup(context);
-    fileCounters.setCounter(BlockKeys.BLOCKS, new MutableLong());
-    fileCounters.setCounter(BlockKeys.WRITE_TIME_WINDOW, new MutableLong());
-  }
-
-  @Override
-  protected void processTuple(AbstractBlockReader.ReaderRecord<Slice> tuple)
-  {
-    long start = System.currentTimeMillis();
-    super.processTuple(tuple);
-    timePerWindow += System.currentTimeMillis() - start;
   }
 
   @Override
@@ -78,14 +69,11 @@ public class BlockWriter extends AbstractFileOutputOperator<AbstractBlockReader.
     super.endWindow();
     streamsCache.asMap().clear();
     endOffsets.clear();
-    fileCounters.getCounter(BlockKeys.BLOCKS).add(blockMetadatas.size());
 
     for (BlockMetadata.FileBlockMetadata blockMetadata : blockMetadatas) {
       blockMetadataOutput.emit(blockMetadata);
     }
     blockMetadatas.clear();
-    fileCounters.getCounter(BlockKeys.WRITE_TIME_WINDOW).setValue(timePerWindow);
-    timePerWindow = 0;
   }
 
   @Override
@@ -102,8 +90,71 @@ public class BlockWriter extends AbstractFileOutputOperator<AbstractBlockReader.
 
   private static final Logger LOG = LoggerFactory.getLogger(BlockWriter.class);
 
-  protected static enum BlockKeys
+  @Override
+  public Collection<Partition<BlockWriter>> definePartitions(Collection<Partition<BlockWriter>> partitions, PartitioningContext context)
   {
-    BLOCKS, WRITE_TIME_WINDOW
+    if (partitions.iterator().next().getStats() == null) {
+      //First time when define partitions is called
+      return partitions;
+    }
+    List<Partition<BlockWriter>> newPartitions = Lists.newArrayList();
+    newPartitions.addAll(partitions);
+    List<BasicCounters<MutableLong>> deletedCounters = Lists.newArrayList();
+
+    int morePartitionsToCreate = context.getParallelPartitionCount() - newPartitions.size();
+    if (morePartitionsToCreate < 0) {
+      //Delete partitions
+      Iterator<Partition<BlockWriter>> partitionIterator = newPartitions.iterator();
+
+      while (morePartitionsToCreate++ < 0) {
+        Partition<BlockWriter> toRemove = partitionIterator.next();
+        deletedCounters.add(toRemove.getPartitionedInstance().fileCounters);
+
+        partitionIterator.remove();
+      }
+    }
+    else {
+      //Add more partitions
+      BlockWriter anOperator = newPartitions.iterator().next().getPartitionedInstance();
+
+      while (morePartitionsToCreate-- > 0) {
+        DefaultPartition<BlockWriter> partition = new DefaultPartition<BlockWriter>(anOperator);
+        newPartitions.add(partition);
+      }
+    }
+
+    //transfer the counters
+    BlockWriter targetWriter = newPartitions.iterator().next().getPartitionedInstance();
+    for (BasicCounters<MutableLong> removedCounter : deletedCounters) {
+      addCounters(targetWriter.fileCounters, removedCounter);
+    }
+    return newPartitions;
+  }
+
+  /**
+   * Transfers the counters in partitioning.
+   *
+   * @param target target counter
+   * @param source removed counter
+   */
+  protected void addCounters(BasicCounters<MutableLong> target, BasicCounters<MutableLong> source)
+  {
+    for (Enum<BlockWriter.Counters> key : BlockWriter.Counters.values()) {
+      MutableLong tcounter = target.getCounter(key);
+      if (tcounter == null) {
+        tcounter = new MutableLong();
+        target.setCounter(key, tcounter);
+      }
+      MutableLong scounter = source.getCounter(key);
+      if (scounter != null) {
+        tcounter.add(scounter.longValue());
+      }
+    }
+  }
+
+  @Override
+  public void partitioned(Map<Integer, Partition<BlockWriter>> partitions)
+  {
+
   }
 }
