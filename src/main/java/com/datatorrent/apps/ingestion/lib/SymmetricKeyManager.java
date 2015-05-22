9bf5fc89c77726fcd929ecf98421df73b3bcb90c
Priyanka Gugale <priyanka@datatorrent.com>
2015-05-22 11:17:51 +0530

    Using raw secrete key generation.

--- src/main/java/com/datatorrent/apps/ingestion/lib/SymmetricKeyManager.java
+++ src/main/java/com/datatorrent/apps/ingestion/lib/SymmetricKeyManager.java
@@ -1,13 +1,7 @@
 package com.datatorrent.apps.ingestion.lib;
 
-import java.io.UnsupportedEncodingException;
 import java.security.Key;
-import java.security.NoSuchAlgorithmException;
-import java.security.spec.InvalidKeySpecException;
-import java.security.spec.KeySpec;
 
-import javax.crypto.SecretKeyFactory;
-import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
 
 /**
@@ -42,8 +36,7 @@ public class SymmetricKeyManager
   /**
    * Generates secret from given key bytes
    *
-   * @param key
-   *          key bytes
+   * @param key key bytes
    */
   public Key generateKey(byte[] key)
   {
@@ -53,27 +46,12 @@ public class SymmetricKeyManager
   /**
    * Generates secret from given key bytes
    *
-   * @param key
-   *          key bytes
-   * @param algorithm
-   *          the name of the secret-key algorithm to be associated with the given key material.
+   * @param key key bytes
+   * @param algorithm the name of the secret-key algorithm to be associated with the given key material.
    */
   public Key generateKey(byte[] key, String algorithm)
   {
-    // return new SecretKeySpec(key, algorithm);
-    try {
-      SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
-      KeySpec keySpec = new PBEKeySpec(CRYPTO_PASSWORD.toCharArray(),"passwordsalt".getBytes("UTF-8"),65536,128);
-      Key genKey = factory.generateSecret(keySpec);
-      Key secret = new SecretKeySpec(genKey.getEncoded(), "AES");
-      return secret;
-    } catch (InvalidKeySpecException e) {
-      throw new RuntimeException(e);
-    } catch (NoSuchAlgorithmException e) {
-      throw new RuntimeException(e);
-    } catch (UnsupportedEncodingException e) {
-      throw new RuntimeException(e);
-    }
+    return new SecretKeySpec(key, algorithm);
   }
 
 }
