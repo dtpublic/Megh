7582b9e3a22ed00aab8ff77bc6be8ceabe1e45ae
Priyanka Gugale <priyanka@datatorrent.com>
2015-06-22 22:01:27 +0530

    SPOI-5183: Setting output folder for message apps as per meger output key

--- src/main/java/com/datatorrent/apps/ingestion/Application.java
+++ src/main/java/com/datatorrent/apps/ingestion/Application.java
@@ -258,6 +258,7 @@ public class Application implements StreamingApplication
     inputOpr.setConsumer(consumer);
 
     BytesFileOutputOperator outputOpr = dag.addOperator("FileWriter", new BytesFileOutputOperator());
+    outputOpr.setFilePath(conf.get("dt.operator.FileMerger.prop.filePath"));
     FilterStreamProvider.FilterChainStreamProvider<FilterOutputStream, OutputStream> chainStreamProvider = new FilterStreamProvider.FilterChainStreamProvider<FilterOutputStream, OutputStream>();
     if (cryptoInfo != null) {
       TimedCipherStreamProvider cipherProvider = new TimedCipherStreamProvider(cryptoInfo.getTransformation(), cryptoInfo.getSecretKey());
@@ -288,6 +289,7 @@ public class Application implements StreamingApplication
     JMSBytesInputOperator inputOpr = dag.addOperator("MessageReader", new JMSBytesInputOperator());
     // Writes to file
     BytesFileOutputOperator outputOpr = dag.addOperator("FileWriter", new BytesFileOutputOperator());
+    outputOpr.setFilePath(conf.get("dt.operator.FileMerger.prop.filePath"));
 
     FilterStreamProvider.FilterChainStreamProvider<FilterOutputStream, OutputStream> chainStreamProvider = new FilterStreamProvider.FilterChainStreamProvider<FilterOutputStream, OutputStream>();
     if (cryptoInfo != null) {
