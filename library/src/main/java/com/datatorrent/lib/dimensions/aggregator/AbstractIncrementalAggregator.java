448a82dafa48981ae6d623640a75580e26c43d1a
Timothy Farkas <tim@datatorrent.com>
2015-10-09 12:45:57 -0700

    - SPOI-6396 #resolve Configured machine data demo for high performance.  - Fixed abstract incremental aggregator so that an arbitrary hash function could be used.

--- library/src/main/java/com/datatorrent/lib/dimensions/aggregator/AbstractIncrementalAggregator.java
+++ library/src/main/java/com/datatorrent/lib/dimensions/aggregator/AbstractIncrementalAggregator.java
@@ -54,12 +54,19 @@ public abstract class AbstractIncrementalAggregator implements IncrementalAggreg
   @Override
   public int computeHashCode(InputEvent inputEvent)
   {
+    long timestamp = inputEvent.getKeys().getFieldsLong()[this.context.inputTimestampIndex];
+
+    if(this.context.inputTimestampIndex != -1 &&
+       this.context.outputTimebucketIndex != -1) {
+      inputEvent.getKeys().getFieldsLong()[this.context.inputTimestampIndex] =
+      this.context.dd.getCustomTimeBucket().roundDown(timestamp);
+    }
+
     int hashCode = GPOUtils.indirectHashcode(inputEvent.getKeys(), context.indexSubsetKeys);
 
-    if(this.context.inputTimestampIndex != -1
-       && this.context.outputTimebucketIndex != -1) {
-      hashCode ^= inputEvent.getKeys().getFieldsLong()[this.context.inputTimestampIndex];
-      hashCode ^= this.context.dd.getCustomTimeBucket().roundDown(inputEvent.getKeys().getFieldsLong()[this.context.inputTimestampIndex]);
+    if(this.context.inputTimestampIndex != -1 &&
+       this.context.outputTimebucketIndex != -1) {
+      inputEvent.getKeys().getFieldsLong()[this.context.inputTimestampIndex] = timestamp;
     }
 
     return hashCode;
