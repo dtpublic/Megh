5a95ad39d32313f0df9253379f5ab878725f2182
Pradeep A. Dalvi <pradeep.dalvi@datatorrent.com>
2015-12-01 18:42:52 +0530

    SPOI-6619: Creation of Laggards Module  - Laggards Operator  - Laggards Module  - Laggards Module Test App

---
+++ demos/laggards-module-app/src/main/java/com/datatorrent/laggards/datagen/SimplePartitioner.java
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) 2015 DataTorrent, Inc.
+ * ALL Rights Reserved.
+ */
+
+package com.datatorrent.demos.laggards.datagen;
+
+import kafka.producer.Partitioner;
+import kafka.utils.VerifiableProperties;
+
+public class SimplePartitioner implements Partitioner
+{
+  public SimplePartitioner(VerifiableProperties props)
+  {
+  }
+
+  public int partition(Object key, int a_numPartitions)
+  {
+    int partition = 0;
+    String stringKey = (String)key;
+    int offset = stringKey.lastIndexOf('.');
+    if (offset > 0) {
+      partition = Integer.parseInt(stringKey.substring(offset + 1)) % a_numPartitions;
+    }
+    return partition;
+  }
+}
