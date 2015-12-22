82f7606155325234a900ef33781a226e33646b1f
chinmaykolhatkar <chinmay@datatorrent.com>
2015-12-22 17:37:50 +0530

    SPOI-7044: Create Expression Evaluator support Java Expression Language
    The expresion are based on java expressions except the operands needs to be registered before and put inside ${}.
    
    The expression evaluator gives options to evaluate a complete function as well as an expression.
    
    The evaluator provides with some basic utilities to be used directly into expression and also give freedom to add a custom method to be registered with ExpressionEvaluator.
    This custom method once registered becomes callable into expression directly.

---
+++ library/src/main/java/com/datatorrent/lib/expressions/DateTimeUtils.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2015 DataTorrent, Inc.
+ * All rights reserved.
+ */
+package com.datatorrent.lib.expressions;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+
+public class DateTimeUtils
+{
+  public Date nowDate()
+  {
+    return new Date();
+  }
+
+  public long nowTime()
+  {
+    return System.currentTimeMillis();
+  }
+
+  public Calendar nowCalender()
+  {
+    return Calendar.getInstance();
+  }
+
+  public Calendar nowCalender(String timeZone)
+  {
+    return Calendar.getInstance(TimeZone.getTimeZone(timeZone));
+  }
+}
---
+++ library/src/main/java/com/datatorrent/lib/expressions/StringUtils.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2015 DataTorrent, Inc.
+ * All rights reserved.
+ */
+package com.datatorrent.lib.expressions;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringUtils
+{
+  public static boolean equalsWithCase(String str1, String str2)
+  {
+    return org.apache.commons.lang3.StringUtils.equals(str1, str2);
+  }
+
+  public static String truncate(String str, int length)
+  {
+    return (str == null) ? null : str.substring(0, length);
+  }
+
+  public static String regexCapture(String str, String regex, int groupId)
+  {
+    if (groupId <= 0) {
+      return null;
+    }
+
+    Pattern entry = Pattern.compile(regex);
+    Matcher matcher = entry.matcher(str);
+
+    return matcher.find() ? matcher.group(groupId) : null;
+  }
+}
