4d257ed76c21326194b402085eb79afb3ecc9a0d
Chandni Singh <csingh@apache.org>
2016-01-07 00:55:53 -0800

    SPOI-7106 #resolve #comment excluding packages from semver and marked them as evolving

--- library/src/main/java/com/datatorrent/lib/appdata/package-info.java
+++ library/src/main/java/com/datatorrent/lib/appdata/package-info.java
@@ -1,5 +1,5 @@
-/*
- * Copyright (c) 2014 DataTorrent, Inc. ALL Rights Reserved.
+/**
+ * Copyright (c) 2015 DataTorrent, Inc. ALL Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,11 +13,5 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-/**
- * This package contains operator which drops duplicate events.<br/>
- * {@link com.datatorrent.lib.dedup.Deduper} group events in buckets and use
- * {@link com.datatorrent.lib.bucket.BucketManager} for better memory management.
- *
- */
-package com.datatorrent.lib.dedup;
+@org.apache.hadoop.classification.InterfaceStability.Evolving
+package com.datatorrent.lib.appdata;
