a92347e7b2a9628fb5f9967318ddbd6249d5c65a
brightchen <bright@datatorrent.com>
2016-03-11 16:34:29 -0800

    SPOI-7665 #resolve #comments Merge Megh enhancement for TelecomDemo

---
+++ contrib/src/main/java/com/datatorrent/contrib/dimensions/CombinationFilter.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) 2016 DataTorrent, Inc.
+ * All rights reserved.
+ */
+package com.datatorrent.contrib.dimensions;
+
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * This interface provides method to filter the input keyToValues
+ *
+ */
+public interface CombinationFilter
+{
+  /**
+   * filter the input keyToValues
+   * @param keyToValues the source keyToValues to be filtered.
+   * @return the filtered keyToValues map
+   */
+  public Map<String, Set<Object>> filter(Map<String, Set<Object>> keyToValues);
+}
